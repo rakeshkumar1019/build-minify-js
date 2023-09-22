b2aaf0a34cec633a7904997e9fa11407a996606e
srakesh1019 <srakeshkumar1019@gmail.com>
2023-09-22 09:07:30 +0530

    generate AST

--- src/processor.js
+++ src/processor.js
@@ -1,5 +1,23 @@
+import fs from "node:fs"
+import { basename } from "node:path";
+import Minifier from "./minifier.js";
 export default class Processor {
+  static generateMinifiedFilePath(filename){
+    return filename.replace('.js','.min.js');
+  }
+  static #generateMinifiedCode ({originalCode,minifiedFilePath,minifiedLocalFilePath}){
+     const minifier = new Minifier();
+     minifier.minifyCodeAndReturnMapNames(originalCode);
+  }
    static run(filename){
+      const originalCode = fs.readFileSync(filename,'utf-8');
+      const minifiedFilePath = this.generateMinifiedFilePath(filename); // public/index.min.js
+      const minifiedLocalFilePath = basename(minifiedFilePath); // index.min.js
+      this.#generateMinifiedCode({
+        originalCode,
+        minifiedFilePath,
+        minifiedLocalFilePath
+      });
       console.log(`Minified code and source map generated with success! ${filename}`)
    }
 }
